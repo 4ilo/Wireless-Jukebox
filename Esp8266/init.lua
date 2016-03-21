bc804fe4be2fbf35dee7d9d9ea0edcb0e78c3571
Olivier <olivier.vandeneede@student.thomasmore.be>
2016-03-21 19:31:27 +0100

    simpele iphone app om te stemmen + json webserver op esp

--- Esp8266/init.lua
+++ Esp8266/init.lua
@@ -1,10 +1,16 @@
-print("Ready to start soft ap AND station")
+print("Wireless jukebox-esp V1.0")
+    
+     json = require "cjson"
+
      local str=wifi.ap.getmac();
-     wifi.setmode(wifi.SOFTAP)       -- instellen als acces point en wifi zoeken
+     wifi.setmode(wifi.STATIONAP)       -- instellen als acces point en wifi zoeken
+
+     wifi.sta.config("Van den Eede","a123456789")
+     print(wifi.sta.getip())
      
      local cfg={}
      cfg.ssid="Wireless jukebox";            -- De acces point instellingen
-     --cfg.pwd="12345678"
+     cfg.pwd="12345678"
      wifi.ap.config(cfg)
      cfg={}
      cfg.ip="192.168.2.1";
@@ -13,7 +19,7 @@ print("Ready to start soft ap AND station")
      wifi.ap.setip(cfg);
 
      uart.setup(0,9600,8,0,1)      -- We initialiseren de uart voor communicatie
-     uart.write(0,"Wireless jukebox webserver V1.0\n")
+     --uart.write(0,"Wireless jukebox webserver V1.0\n")
 
      --testfunctie voor uart receive
 
@@ -91,18 +97,44 @@ srv:listen(80,function(conn)
             end
         end
         
-        -- We openen de file en sturen het lijn per lijn naar de client
-        file.open('webpagina.html','r');
-        local line = file.readline();
-        while (line) do
-            line = checkIfSongTitle(line);      -- We kijken of er een titel moet komen, 
-            client:send(line);                  --  en vervanen als het nodig is
-            line = file.readline();
-        end
-        file.close();
+        -- Eerst kijken we of er een get request is om titels op te vragen
+        if(_GET.titels) then
 
-        count(_GET.next);
-        printSongs();
+            conn:send("HTTP/1.0 200 OK\n")
+            conn:send("Server: ESP (nodeMCU)\n")
+            conn:send("Content-Type: application/json")
+            conn:send("Content-Length: 1\n\n")
+            
+            client:send('[');
+            client:send('{ "Titel" : "test' .. songTitle1 .. ' "},');
+            client:send('{ "Titel" : "' .. songTitle2 .. ' "},');
+            client:send('{ "Titel" : "' .. songTitle3 .. ' "},');
+            client:send('{ "Titel" : "' .. songTitle4 .. ' "}' );
+            client:send(']');
+ 
+            --local superHeroes = {
+            --    { name="Clark Kent", nickname="Superman", address="Metropolis", age=32 },
+            --    { name="Bruce Wayne", nickname="Batman", address="Gotham", age=36 },
+            --    { name="Diana Prince", nickname="Wonder Woman", address="New York", age=28 }
+            --    }
+            --    conn:send(json.encode(superHeroes))
+
+            --conn:send("a")
+        else
+            -- We openen de file en sturen het lijn per lijn naar de client
+            file.open('webpagina.html','r');
+            local line = file.readline();
+            while (line) do
+                line = checkIfSongTitle(line);      -- We kijken of er een titel moet komen, 
+                client:send(line);                  --  en vervanen als het nodig is
+                line = file.readline();
+            end
+            file.close();
+
+            count(_GET.next);
+            printSongs();
+
+        end
         
         client:close();
         collectgarbage();
