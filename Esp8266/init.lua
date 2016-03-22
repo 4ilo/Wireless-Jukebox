35259ab632ce1eb1b9ca7bd8d95fef421c7ee2a2
Olivier <olivier.vandeneede@student.thomasmore.be>
2016-03-22 12:50:24 +0100

    esp code is zo goed als klaar

--- Esp8266/init.lua
+++ Esp8266/init.lua
@@ -1,44 +1,63 @@
 print("Wireless jukebox-esp V1.0")
 
-     local str=wifi.ap.getmac();
-     wifi.setmode(wifi.STATIONAP)       -- instellen als acces point en wifi zoeken
-
-     wifi.sta.config("Van den Eede","a123456789")
-     print(wifi.sta.getip())
-     
-     local cfg={}
-     cfg.ssid="Wireless jukebox";            -- De acces point instellingen
-     cfg.pwd="12345678"
-     wifi.ap.config(cfg)
-     cfg={}
-     cfg.ip="192.168.2.1";
-     cfg.netmask="255.255.255.0";
-     cfg.gateway="192.168.2.1";
-     wifi.ap.setip(cfg);
-
-     uart.setup(0,9600,8,0,1)      -- We initialiseren de uart voor communicatie
-     --uart.write(0,"Wireless jukebox webserver V1.0\n")
-
-     --testfunctie voor uart receive
-
-    function data( data )
-        if(data ~= nil) then
-            print( data .. " received")
-        else
-            print("nil")
-        end
-    end
-
 -- De globale variabelen om het aantal stemmen bij te houden
 song1 = 0
 song2 = 0
 song3 = 0
 song4 = 0
 
-songTitle1 = "Missing - EDX"
-songTitle2 = "Faded - Alan Walker"
-songTitle3 = "Perfect - One Direction"
-songTitle4 = "Sorry - Justin Bieber"
+songTitle1 = ""
+songTitle2 = ""
+songTitle3 = ""
+songTitle4 = ""
+
+-- Deze functie zet de juiste liedjes in de variabelen
+function setSong(nr, titel)
+    if(titel ~= nil) then
+        if(nr == 1) then
+        	songTitle1 = titel
+        elseif(nr == 2) then
+          	songTitle2 = titel
+        elseif(nr == 3) then
+           	songTitle3 = titel
+        elseif(nr == 4) then
+           	songTitle4 = titel
+        else
+           	uart.write(0,"ERROR")
+        end
+    end
+end
+
+-- Returnt het meest gestemde liedjesId
+function getBest()
+	local lijst = {}
+	lijst[1] = song1
+	lijst[2] = song2
+	lijst[3] = song3
+	lijst[4] = song4
+	table.sort(lijst)
+
+	if(lijst[4] == song1) then
+		uart.write(0,"1\n")
+	elseif(lijst[4] == song2) then
+		uart.write(0,"2\n")
+	elseif(lijst[4] == song3) then
+		uart.write(0,"3\n")
+	elseif(lijst[4] == song4) then
+		uart.write(0,"4\n")
+	end
+
+	resetSongs()
+
+end
+
+-- Functie zet alle tellers terug op 0
+function resetSongs()
+	song1 = 0
+	song2 = 0
+	song3 = 0
+	song4 = 0
+end
 
 -- Een functie die bijhoud hoeveel er op elk liedje gestemd wordt
 function count( vote )
@@ -74,12 +93,30 @@ function checkIfSongTitle( line )
     end
     return line;
 end
+
+local str=wifi.ap.getmac();
+--wifi.setmode(wifi.STATIONAP)       -- instellen als acces point en wifi zoeken
+wifi.setmode(wifi.SOFTAP)
+
+--wifi.sta.config("Van den Eede","a123456789")
+     
+local cfg={}
+cfg.ssid="Wireless jukebox";            -- De acces point instellingen
+cfg.pwd="12345678"
+wifi.ap.config(cfg)
+cfg={}
+cfg.ip="192.168.2.1";
+cfg.netmask="255.255.255.0";
+cfg.gateway="192.168.2.1";
+wifi.ap.setip(cfg);
+
+uart.setup(0,9600,8,0,1)      -- We initialiseren de uart voor communicatie
+     --uart.write(0,"Wireless jukebox webserver V1.0\n")
     
 -- We starten de server op op poort 80
 srv=net.createServer(net.TCP)
 srv:listen(80,function(conn)
     conn:on("receive", function(client,request)
-        local buf = "";
         local _, _, method, path, vars = string.find(request, "([A-Z]+) (.+)?(.+) HTTP");
         if(method == nil)then
             _, _, method, path = string.find(request, "([A-Z]+) (.+) HTTP");
@@ -90,41 +127,39 @@ srv:listen(80,function(conn)
                 _GET[k] = v
             end
         end
-
-        -- We sturen sowiso de http header
-        conn:send("HTTP/1.0 200 OK\n")
-        conn:send("Server: ESP (nodeMCU)\n")
-        conn:send("Content-Length: 1\n\n")
         
         -- Eerst kijken we of er een get request is om titels op te vragen
         if(_GET.titels) then
             
-            conn:send("Content-Type: application/json")		-- Het is een json response
+            --conn:send("HTTP/1.0 200 OK\n")
+        	--conn:send("Server: ESP (Wireless jukebox)\n")
+        	--conn:send("Content-Length: 1\n")
+            --conn:send("Content-Type: application/json\n")		-- Het is een json response
+            
             
             client:send('[');
-            client:send('{ "Titel" : "test' .. songTitle1 .. ' "},');
-            client:send('{ "Titel" : "' .. songTitle2 .. ' "},');
-            client:send('{ "Titel" : "' .. songTitle3 .. ' "},');
-            client:send('{ "Titel" : "' .. songTitle4 .. ' "}' );
-            client:send(']');
+            client:send('{ "Titel" : "' .. songTitle1 .. ' "},')
+            client:send('{ "Titel" : "' .. songTitle2 .. ' "},')
+            client:send('{ "Titel" : "' .. songTitle3 .. ' "},')
+            client:send('{ "Titel" : "' .. songTitle4 .. ' "}' )
+            client:send(']')
 
         else
             -- We openen de file en sturen het lijn per lijn naar de client
-            file.open('webpagina.html','r');
-            local line = file.readline();
+            --conn:send('HTTP/1.1 200 OK\n\n')
+            --conn:send("Server: ESP (Wireless jukebox)\n\n")
+            file.open('webpagina.html','r')
+            local line = file.readline()
             while (line) do
-                line = checkIfSongTitle(line);      -- We kijken of er een titel moet komen, 
-                client:send(line);                  --  en vervanen als het nodig is
-                line = file.readline();
+                line = checkIfSongTitle(line)      -- We kijken of er een titel moet komen, 
+                client:send(line)                  --  en vervanen als het nodig is
+                line = file.readline()
             end
-
-            file.close();
-
-            count(_GET.next);		-- We tellen het aantal stemmen
-            printSongs();
-
+            file.close()
+            count(_GET.next)		-- We tellen het aantal stemmen
+            printSongs()
         end
-        
+        --client:on("sent", function(client) client:close() end)
         client:close();
         collectgarbage();
     end)
