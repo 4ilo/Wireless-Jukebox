0478ce6ad690874fe518964269e61946df9bbafc
Olivier <olivier.vandeneede@student.thomasmore.be>
2016-04-30 16:54:04 +0200

    functies om uit wav te lezen + muziek uit wav begonnen

--- Wireless jukebox prog/Inc/wav.h
+++ Wireless jukebox prog/Inc/wav.h
@@ -1,5 +1,10 @@
 
 #include "fatfs.h"
+#include <string.h>
+#include <stdlib.h>
 
 
 uint16_t getTitels(FIL * file, char ** titels, uint16_t maxTitels);
+uint8_t getData(FIL * fp, char data[],int aantal);
+
+extern UART_HandleTypeDef huart2;           // De uart typedef in de main
--- Wireless jukebox prog/Src/wav.c
+++ Wireless jukebox prog/Src/wav.c
@@ -27,3 +27,51 @@ uint16_t getTitels(FIL * file, char ** titels, uint16_t maxTitels)
 	return teller;
 }
 
+
+//
+//  We gaan data opvragen uit de wav file
+//
+uint8_t getData(FIL * fp, char data[],int aantal)
+{
+    int teller = 0;
+    static uint8_t dataGevonden = 0;
+
+    char byte;
+    uint8_t bytesRead;
+
+    uint16_t i;
+
+    // eerst moeten we de header over springen tot aan de data chunck
+
+    while(f_eof(fp) == 0)            // we lezen tot aan het einde van de file
+    {
+    	f_read(fp,&byte,1 * sizeof(char),(UINT*)&bytesRead);
+        if(dataGevonden == 0)                       // Kijken of we de data header al gevonden hadden
+        {
+            if(byte == 'd')         // we hebben een d gevonden, nu zoeken we naar "ata" van data
+            {
+                //fgets(keyword,4,fp);
+            	f_read(fp,&byte,1 * sizeof(char),(UINT*)&bytesRead);
+                if(byte == 'a')
+                {
+                    dataGevonden = 1;
+
+                    for(i=0; i<6;i++)
+                    {
+                    	f_read(fp,&byte,1 * sizeof(char),(UINT*)&bytesRead);  // De volgende 6 bytes zijn nog onbelangrijk
+                    }
+                }
+            }
+        }
+        else                                // de data header is gevonden, vanaf nu komt de data
+        {
+            data[teller] = byte;            // We slagen het aantal gevraagde databytes op in de array
+            teller ++;
+            if(teller == aantal)
+            {
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
